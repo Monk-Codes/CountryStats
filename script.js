0e00d5319ceeedcd0abaaedb6cbd6e7f7a847265
dizzy <84877191+drpratikk@users.noreply.github.com>
2023-07-07 22:16:33 +0530

    search history update

--- script.js
+++ script.js
@@ -2,14 +2,29 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-const searchHistoryList = document.querySelector('.search-history');
+const searchHistoryList = document.querySelector('.search-history-list');
+const searchHistoryTab = document.querySelector('.search-history-tab');
+let isSearchHistoryOpen = false;
 
 let searchHistory = [];
+const loadSearchHistory = function () {
+  const storedSearchHistory = localStorage.getItem('searchHistory');
+  if (storedSearchHistory) {
+    searchHistory = JSON.parse(storedSearchHistory);
+  }
+};
+loadSearchHistory();
+
+// Call loadSearchHistory function to display saved data on page load
 
 ///////////////////////////////////////
 const country = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      return response.json();
+    })
     .then(data => {
       const [countryData] = data;
       console.log(countryData);
@@ -17,16 +32,14 @@ const country = function (country) {
       let laNg = countryData.languages[Object.keys(countryData.languages)[0]];
       let maps = countryData.maps[Object.keys(countryData.maps)[0]];
       const html = `
-        <article class="country">
-          <img class="country__img" src="${countryData.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${countryData.name.official}</h3>
-            <h3 class="country__capital">Capital is ${
-              countryData.capital[0]
-            }</h3>
-            <h4 class="country__region">It lies in continent of ${
-              countryData.continents
-            }</h4>
+      <article class="country">
+      <img class="country__img" src="${countryData.flags.png}" />
+      <div class="country__data">
+      <h3 class="country__name">${countryData.name.official}</h3>
+      <h3 class="country__capital">Capital is ${countryData.capital[0]}</h3>
+      <h4 class="country__region">It lies in continent of ${
+        countryData.continents
+      }</h4>
             <p class="country__row"><span>👫</span>${(
               +countryData.population / 1000000
             ).toFixed(1)}M people</p>
@@ -34,13 +47,25 @@ const country = function (country) {
             <p class="country__row"><span>💰</span>${curr.name}</p>
             <p class="maps-link"><span>🗺️</span><a href="${maps}" target="_blank">Get Map</a></p>
             </div>
-        </article>
+            </article>
       `;
       countriesContainer.insertAdjacentHTML('beforeend', html);
+    })
+    .catch(err => {
+      if (err.message.includes('Country not found')) {
+        // Country not found, display an alert
+        alert('Country not found!');
+      } else {
+        // Other errors, display a generic error message
+        renderError(`Something went wrong (${err.message}). Please try again.`);
+      }
+    })
+    .finally(() => {
+      // Update search history after the promise is settled
+      searchHistory.push(country);
+      updateSearchHistory();
+      saveSearchHistory();
     });
-  // Update search history
-  searchHistory.push(country);
-  updateSearchHistory();
 };
 
 btn.addEventListener('click', function () {
@@ -63,6 +88,19 @@ btn.addEventListener('click', function () {
     }
   });
 });
+
+const toggleSearchHistory = function () {
+  if (isSearchHistoryOpen) {
+    searchHistoryTab.classList.remove('open');
+  } else {
+    searchHistoryTab.classList.add('open');
+  }
+  isSearchHistoryOpen = !isSearchHistoryOpen;
+};
+
+// Add click event listener to the search history tab
+searchHistoryTab.addEventListener('click', toggleSearchHistory);
+
 const updateSearchHistory = function () {
   // Clear the existing search history
   searchHistoryList.innerHTML = '';
@@ -73,4 +111,8 @@ const updateSearchHistory = function () {
     listItem.textContent = search;
     searchHistoryList.appendChild(listItem);
   });
+};
+const saveSearchHistory = function () {
+  // Save search history to local storage
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 };
